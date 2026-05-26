e6230a2390d4d46c75d5b698c815793a618cba67
uruk@Rafis-MBP-2.(none) <ahmed.rimawi@taifco.net>
2026-05-27 01:28:27 +0400

    fix database env

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
-from sqlalchemy import create_engine
+import os 
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 #DATABASE_URL = "postgresql://uruk:123456@localhost/client_dashboard"
-DATABASE_URL = "postgresql://postgres:!39QHA&U-*/qPPf@db.ypyryqegoarpdeztpddy.supabase.co:5432/postgres"
+#DATABASE_URL = "postgresql://postgres:!39QHA&U-*/qPPf@db.ypyryqegoarpdeztpddy.supabase.co:5432/postgres"
+#DATABASE_URL = "postgresql://postgres:!39QHA&U-*/qPPf@db.ypyryqegoarpdeztpddy.supabase.co:5432/postgres"
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
