9c64cd3fe4438f7d4a637db1bfa89ad2a0946b6d
uruk@Rafis-MBP-2.(none) <ahmed.rimawi@taifco.net>
2026-05-27 01:41:39 +0400

    fix db engine

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,pool_pre_ping=True, connect_args={ "sslmode": "require" })
 
 SessionLocal = sessionmaker(
     autocommit=False,
