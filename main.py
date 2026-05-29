a0f925bd993d4dcb13ce9b7fffb2099e445688fc
uruk@Rafis-MBP-2.(none) <ahmed.rimawi@taifco.net>
2026-05-29 11:03:13 +0400

    add root route

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "API is running"}
+
 # @app.on_event("startup")
 # def startup():
 #     try:
